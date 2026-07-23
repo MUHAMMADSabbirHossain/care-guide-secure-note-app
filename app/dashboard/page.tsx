9895cfd34d15ff71b2357b957b32d55886db3a2c
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-07-23 23:44:52 +0600

    feat: add LogoutBtn component and refactor dashboard to use it.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import LogoutBtn from "@/components/LogoutBtn";
 
 export default function DashboardPage() {
   const router = useRouter();
@@ -28,12 +29,6 @@ export default function DashboardPage() {
     fetchUser();
   }, [router]);
 
-  const handleLogout = async () => {
-    await fetch("/api/logout", { method: "POST" });
-    router.push("/login");
-    router.refresh();
-  };
-
   if (loading) return <div className="p-8">Loading...</div>;
 
   return (
@@ -46,12 +41,8 @@ export default function DashboardPage() {
           ? new Date(user.lastLogin).toLocaleString()
           : "First time"}
       </p>
-      <button
-        onClick={handleLogout}
-        className="mt-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
-      >
-        Logout
-      </button>
+
+      <LogoutBtn />
     </div>
   );
 }
---
+++ components/LogoutBtn.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const LogoutBtn = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await fetch("/api/logout", { method: "POST" });
+    router.push("/login");
+    router.refresh();
+  };
+
+  return (
+    <div>
+      {" "}
+      <button
+        onClick={handleLogout}
+        className="mt-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default LogoutBtn;
